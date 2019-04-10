6cedb14a4db57c136171567283a9d210daa4ef2d
donia <donia@MacBook-Pro-de-Donia.local>
2019-04-10 15:04:54 +0200

    second try

--- test.py
+++ test.py
@@ -6,10 +6,15 @@ from distutils.dir_util import copy_tree
 import shutil
 
 repo = git.Repo(os.getcwd())
-g = git.cmd.Git(os.getcwd())
-g.pull()
+index = git.Repo.init(os.getcwd()).index
+# g = git.cmd.Git(os.getcwd())
+# g.pull()
 #o = repo.remotes.origin
-#repo.git.pull('HEAD')
+repo.git.pull(os.getcwd())
+repo.git.add(u=True)
+index.commit("second try")
+#repo.git.commit()
+#repo.git.push('origin', 'hello')
 
 # repo_date = datetime.now() - relativedelta(days = 7)
 # repo_date = repo_date.strftime("-%d-%m-%Y")
